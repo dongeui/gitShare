e4b75abf477a98ab68585b016a6de8e37c996a64
dongeui <jde8993@naver.com>
2017-05-17 17:42:29 +0900

    자동차, 비행기 AR, 폰으로 동영상 재생하는 씬 3가지.

---
+++ ARTest/Assets/Scirpts/CarColorChange.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CarColorChange : MonoBehaviour
+{
+    public Material mat = null;
+
+    public void ColorChange(Image img)
+    {
+        mat.color = img.color;
+    }
+}
